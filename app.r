44f9fe2085282cf43c991b0acc97f9b16ffc99d0
Nicolás Fernandez <100044816+Dr6k@users.noreply.github.com>
2025-03-14 12:21:44 -0300

    Update app.r

--- app.r
+++ app.r
@@ -1,12 +1,11 @@
 library(shiny)
 library(leaflet)
-library(leaflet.extras)  # Librería necesaria para hacer marcadores arrastrables
+library(leaflet.extras)  
 library(DBI)
 library(pool)
 library(RPostgres)
 library(dplyr)
 
-# Función para obtener ubicaciones desde la BD
 get_ubicaciones <- function() {
   dbGetQuery(pool, "SELECT id, nombre, longitud, latitud FROM ubicaciones")
 }
@@ -22,7 +21,6 @@ ui <- fluidPage(
 # Server
 server <- function(input, output, session) {
   
-  # Cargar ubicaciones de la BD
   boyas <- reactiveVal(get_ubicaciones())
   
   output$mapa_boyas <- renderLeaflet({
@@ -34,11 +32,10 @@ server <- function(input, output, session) {
         layerId = ~id,
         label = ~nombre,
         icon = makeAwesomeIcon(icon = "map-marker", markerColor = "blue"),
-        options = markerOptions(draggable = TRUE)  # ¡Ahora sí funciona!
+        options = markerOptions(draggable = TRUE) 
       )
   })
   
-  # Detectar cuando se mueve un marcador
   observeEvent(input$mapa_boyas_marker_dragend, {
     info <- input$mapa_boyas_marker_dragend
     
@@ -47,22 +44,18 @@ server <- function(input, output, session) {
       nueva_lat <- info$lat
       nueva_lng <- info$lng
       
-      # Mostrar coordenadas en la consola
       output$coordenadas <- renderText({
         paste("Boya", id_boya, "movida a:", nueva_lat, nueva_lng)
       })
       
-      # Actualizar en la base de datos
       query <- sprintf(
         "UPDATE ubicaciones SET latitud = %f, longitud = %f WHERE id = %d",
         nueva_lat, nueva_lng, id_boya
       )
       dbExecute(pool, query)
       
-      # Actualizar datos reactivos
       boyas(get_ubicaciones())
       
-      # Refrescar el mapa
       leafletProxy("mapa_boyas") %>%
         clearMarkers() %>%
         addAwesomeMarkers(
